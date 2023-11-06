fb68dd3575d688734a698dfbf3b1fe8938162212
Fayshal Bin Amir <131062651+fayshal69@users.noreply.github.com>
2023-11-07 01:02:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const input_box = document.querySelector("#input_box");
+const listContainer = document.querySelector("#list_container");
+
+function addTask() {
+    if(input_box.value === '' || input_box.value.trim() === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input_box.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input_box.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
